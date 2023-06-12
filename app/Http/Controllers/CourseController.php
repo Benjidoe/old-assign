3dd402afb0e304890b87294e76e2792660ac369a
Ben-doe <doeocansey002@gmail.com>
2023-06-12 10:10:04 +0000

    pagination

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('courses.index')
+        ->with('courses', Course::paginate());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('courses.form');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=>['required', 'max:100', 'min:4'],
+            'courseCode'=>['required', 'max:20', 'min:4'],
+            'description'=>['required', 'max:100', 'min:4'],
+        ]);
+
+        Course::create($request->only(['name','courseCode','description']));
+        return redirect(route('course.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $course = Course::findOrFail($id);
+        return view('courses.show')
+        ->with('course', $course );
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\StudentController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+
+Route::prefix('/students')->group(function () {
+    Route::get('', [StudentController::class, 'index'])->name('student.index');
+    Route::get('/create', [StudentController::class, 'create'])->name('student.add-form');
+    Route::post('/create', [StudentController::class, 'store'])->name('student.store');
+    Route::get('/edit/{id}', [StudentController::class, 'edit'])->name('student.edit-form');
+    Route::get('/{id}', [StudentController::class, 'show'])->name('student.show');
+    Route::patch('/{id}', [StudentController::class, 'update'])->name('student.update');
+    Route::delete('/{id}', [StudentController::class, 'destroy'])->name('student.delete');
+});
+
+Route::prefix('/courses')->group(function () {
+    Route::get('', [CourseController::class, 'index'])->name('course.index');
+    Route::get('/create', [CourseController::class, 'create'])->name('course.add-form');
+    Route::post('/create', [CourseController::class, 'store'])->name('course.store');
+    Route::get('/edit/{id}', [CourseController::class, 'edit'])->name('course.edit-form');
+    Route::get('/{id}', [CourseController::class, 'show'])->name('course.show');
+    Route::patch('/{id}', [CourseController::class, 'update'])->name('course.update');
+    Route::delete('/{id}', [CourseController::class, 'destroy'])->name('course.delete');
+});
+
+require __DIR__.'/auth.php';
